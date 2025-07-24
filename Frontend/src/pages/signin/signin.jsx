6736eb6237b2692408e1a82507e068112fb0f47f
umeshatharindibandara <umeshatharindi2020@gmail.com>
2025-07-24 16:34:59 +0530

    Update sign in

--- Frontend/src/pages/signin/signin.jsx
+++ Frontend/src/pages/signin/signin.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import './SignIn.css';
+import React from "react";
+import "./signin.css";
+import logo from "../../assets/orelit-logo.png";
 
 const SignIn = () => {
   return (
     <div className="signin-container">
       <div className="left-panel">
-        <div className="logo">OREL <span className="it">IT</span></div>
+        <div className="logo">
+          <img src={logo} alt="orelit" />
+        </div>
         <div className="rms">RMS</div>
         <div className="subtitle">Recruitment Management System</div>
       </div>
       <div className="right-panel">
         <h2 className="signin-title">Sign In</h2>
+        <br />
+        <br />
         <div className="signin-box">
           <input type="email" placeholder="Email" className="input-field" />
-          <input type="password" placeholder="Password" className="input-field" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-field"
+          />
           <button className="login-btn">Login</button>
           <p className="forgot-password">Forgot Password ?</p>
         </div>
@@ -23,4 +32,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
